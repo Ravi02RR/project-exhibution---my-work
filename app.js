b671c47030c876974ec314850ec619d06ab5ad9e
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-07-20 23:26:39 +0530

    a

--- app.js
+++ app.js
@@ -12,84 +12,84 @@ mongoose.connect("mongodb+srv://Ravi02rr:slrbkMeyLMxjBfs3@cluster0.pa8zqtm.mongo
     .catch((e) => console.log(e));
 
 
-app.set('view engine', 'ejs');
+app.set('views engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const replySchema = new mongoose.Schema({
-  reply: String,
+    reply: String,
 });
 
 const Reply = mongoose.model('Reply', replySchema);
 
 const questionSchema = new mongoose.Schema({
-  title: String,
-  replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }],
+    title: String,
+    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Reply' }],
 });
 
 const Question = mongoose.model('Question', questionSchema);
 
 app.get('/', async (req, res) => {
-  try {
-    const questions = await Question.find({});
-    res.render('home', { questions });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const questions = await Question.find({});
+        res.render('home', { questions });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/view', async (req, res) => {
-  try {
-    const questions = await Question.find({});
-    res.render('home', { questions });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const questions = await Question.find({});
+        res.render('home', { questions });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/new', (req, res) => {
-  res.render('ask_que');
+    res.render('ask_que');
 });
 
 app.post('/', async (req, res) => {
-  const title = req.body.question;
-  try {
-    const ask = new Question({ title });
-    await ask.save();
-    res.redirect('/');
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
+    const title = req.body.question;
+    try {
+        const ask = new Question({ title });
+        await ask.save();
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/view/:id', async (req, res) => {
     try {
-      const question = await Question.findById(req.params.id).populate('replies');
-      res.render('question', { question }); // Pass the 'question' variable to the 'question.ejs' template
+        const question = await Question.findById(req.params.id).populate('replies');
+        res.render('question', { question }); // Pass the 'question' variable to the 'question.ejs' template
     } catch (err) {
-      console.log(err);
-      res.status(500).send('Internal Server Error');
+        console.log(err);
+        res.status(500).send('Internal Server Error');
     }
-  });
+});
 
 app.post('/view/:id/replies', async (req, res) => {
-  const replyText = req.body.reply;
-  try {
-    const reply = new Reply({ reply: replyText });
-    await reply.save();
-    const question = await Question.findById(req.params.id);
-    question.replies.push(reply);
-    await question.save();
-    res.redirect(`/view/${req.params.id}`);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
+    const replyText = req.body.reply;
+    try {
+        const reply = new Reply({ reply: replyText });
+        await reply.save();
+        const question = await Question.findById(req.params.id);
+        question.replies.push(reply);
+        await question.save();
+        res.redirect(`/view/${req.params.id}`);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
